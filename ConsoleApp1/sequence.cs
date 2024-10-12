d16fd807a879ed4c6b3813b7b6103776f9d09633
Joseph <jj.selfie@gmail.com>
2024-10-12 11:25:58 -0400

    Add project files.

---
+++ ConsoleApp1/sequence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Sequence
+{
+    
+    public int ValidateSequence(int input1,  int[] input2)
+    {
+
+
+    }
+
+}
